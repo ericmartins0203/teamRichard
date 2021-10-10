c5e392780da3b52567490afae51edd7461f094a5
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-10-09 21:00:56 -0300

    feat:routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Routes } from "./Pages/Routes";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
